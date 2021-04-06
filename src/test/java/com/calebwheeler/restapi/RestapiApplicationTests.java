97823721af380ca765fda996d9c53d83631f2dcb
Caleb Wheeler <caleb.edward.wheeler@gmail.com>
2021-04-06 14:03:10 -0500

    feat: setup new project structure. Originally did not structure the Spring Intializer correctly.

---
+++ src/test/java/com/calebwheeler/restapi/RestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calebwheeler.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
